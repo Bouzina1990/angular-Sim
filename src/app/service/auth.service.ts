72ef546946453cfc6a22bbd37abf4a158e901208
Bouzina1990 <s_chergui@esi.dz>
2020-02-03 10:16:30 +0100

    1 file modified

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -8,12 +8,10 @@ export class AuthService {
   signIn() {
     return new Promise(
       (resolve, reject) => {
-        setTimeout(
-          () => {
+       
             this.isAuth = true;
             resolve(true);
-          }, 2000
-        );
+           
       }
     );
   }
